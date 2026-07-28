f4f5251c959b483a8d7d666a8af54c3d8b708280
Ape-programmer <abiolapeace9@gmail.com>
2026-07-28 18:54:38 +0100

    Add complete voting system source code

---
+++ src/footer.php
@@ -0,0 +1,12 @@
+
+<footer class="footer">
+	<hr/>
+&copy2024. Developed BY ABIOLA PEACE EMMANUEL  : ESPAM/CSC/21/0167
+<br/>
+ONLINE VOTING SYSTEM.
+<br/>
+Final Year Project 2024. ESPAM FORMATION UNIVERSITY BENIN REPUBLIC
+<br/>
+Contact: <a href="tel:+229 41287856">+234 9133987515</a> | Email: <a href="mailto:abiolapeace9@gmail.com">abiolapeace9@gmail.com</a>
+
+</footer>
